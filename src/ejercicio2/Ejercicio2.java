6c1c607e40469a57054c3a38812325c2937edd13
Estudiante <Estudiante@10.203.3.187>
2016-08-09 14:45:02 -0500

    HOLA MUNDO

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 
+ * @author Duver Andrés Herrera Torres
+ * 
+ */
+public class Ejemplo1
+	{
+	public static void main(String []arg)
+		{
+		System.out.println("*Hola mundo*");
+		}
+	}
