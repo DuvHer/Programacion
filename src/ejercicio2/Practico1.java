62b1b65205526355a99fc53342939bfcb9986768
Estudiante <Estudiante@10.203.3.187>
2016-08-09 15:01:15 -0500

    HOLA MUNDO

---
+++ src/ejercicio2/Practico1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * 
+ * @author Duver Andrés Herrera Torres
+ * 
+ */
+
+class shuffle 
+	{
+	public static void main(String []args)
+		{
+                    int x =3;
+                    x=x-1;
+		System.out.print("-");
+                
+                if (x>2){
+                System.out.print("a")
+                }
+                if (x==2)
+                    {
+                    System.out.print("a")
+                    }
+		
+	}
